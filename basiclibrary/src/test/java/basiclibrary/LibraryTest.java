5d9120154f1da0000449525cbf59c36109898993
daesy <daesyramos@hotmail.com>
2020-01-12 03:08:05 -0800

    lab 2 completed

--- basiclibrary/src/test/java/basiclibrary/LibraryTest.java
+++ basiclibrary/src/test/java/basiclibrary/LibraryTest.java
@@ -16,20 +16,43 @@ public class LibraryTest {
     }
 
     @Test public void rollBetween1and6(){
-        //
+        int[] expected = Library.roll(4);
+        for(int num : expected){
+            boolean actual = num > 0 && num < 7;
+            assertTrue(actual);
+        }
     }
 
     @Test public void testContainsDuplicates(){
         String[] input = {"a", "b", "c", "a"};
         boolean actual = Library.containsDuplicates(input);
-        boolean result = true;
-        assertEquals(actual, result);
+        boolean expected = true;
+        assertEquals(actual, expected);
     }
 
     @Test public void testNoContainDuplicates(){
         String[] input = {"a", "b", "c"};
         boolean actual = Library.containsDuplicates(input);
-        boolean result = false;
-        assertEquals(actual, result);
+        boolean expected = false;
+        assertEquals(actual, expected);
+    }
+
+    @Test public void testCalculatingAverage(){
+        int[] input = {1, 2, 3, 4, 5};
+        int actual = Library.calculatingAverage(input);
+        int expected = 3;
+        assertEquals(actual,expected);
+    }
+
+    @Test public void testArrayAverage(){
+        int[][] input = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+        int actual = Library.arrayAverage(input);
+        int expected = 57;
+        assertEquals(actual, expected);
     }
 }
