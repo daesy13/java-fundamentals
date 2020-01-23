b1620529e319cb8657236388387f8261428a5dcc
daesy <daesyramos@hotmail.com>
2020-01-23 10:28:55 -0800

    inheritance2 test

--- inheritance/src/test/java/inheritance/RestaurantTest.java
+++ inheritance/src/test/java/inheritance/RestaurantTest.java
@@ -1,8 +1,28 @@
 package inheritance;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class RestaurantTest {
+
+    Restaurant casaBonita;
+
+    // http://junit.sourceforge.net/javadoc/org/junit/Before.html
+    @Before public void initialize(){
+
+    }
+
     @Test public void restaurantTest(){
     }
+
+    @Test public void addReviewTest (){
+//        Review reviewAtThree = casaBonita.getReview(2);
+//        String expectedMessage1 = "I like to drink nacho cheese.";
+//        String actualMessage1 = reviewAtThree.getReviewMessage();
+//
+//        assertEquals(expectedMessage1, actualMessage1);
+
+    }
+
+
 }
